2da2aad2422318407a68e9c7a567776d690dd03a
Rajprakashkarimsetti <rajprakash.karimsetti@gmail.com>
2024-08-07 13:35:31 +0530

    added some changes in directory

--- frontend/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import transactionReducer from './transactionReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  transactions: transactionReducer,
-});
